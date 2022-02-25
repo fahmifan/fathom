d9c2f480773257ccf4fbeb23335aa643b4e29115
fahmifan <fahmifan173@gmail.com>
2022-02-25 16:05:24 +0700

    feat: refactor & fix a11y
    * use css variable to uniform
    * a11y lack of contrast

--- assets/src/js/components/SiteSettings.js
+++ assets/src/js/components/SiteSettings.js
@@ -117,7 +117,7 @@ class SiteSettings extends Component {
 
                     <fieldset style={props.site.unsaved ? 'display: none;' : ''}>
                         <label>Add this code to your website    <small class="right">(site ID = {props.site.trackingId})</small></label>
-                        <textarea ref={this.setTextarea} onFocus={this.handleTextareaClickEvent} readonly="readonly">{`<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+                        <textarea  class="client-script" ref={this.setTextarea} onFocus={this.handleTextareaClickEvent} readonly="readonly">{`<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
 <script>
 (function(f, a, t, h, o, m){
 	a[h]=a[h]||function(){
